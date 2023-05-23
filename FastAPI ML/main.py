a252db34e6a416437ba9b3ccdd8ef3d4fb8d3e70
shahmananv13 <shah.manan.vinod@gmail.com>
2023-05-23 11:50:02 +0530

    Added pdf only input parameter

--- FastAPI ML/main.py
+++ FastAPI ML/main.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import string
+from fastapi.exceptions import HTTPException
+
 
 app = FastAPI()
 
-@app.post("/")
-async def upload_pdf(pdf_file: UploadFile = File(...)):
-    contents = await pdf_file.read()
-    # Perform further processing or save the PDF file
-    return {"filename": pdf_file.filename}
+@app.post("/upload")
+async def upload_pdf(file: UploadFile):
+	print(file.content_type)
+	if file.content_type != 'application/pdf':
+		raise HTTPException(400, 'Not a valid pdf file')
+	return {"filetype": file.filename}
+
+
+if __name__ == '__main__':
+	uvicorn.run('main:app', host = "127.0.0.1", port = 8080, reload = True)
